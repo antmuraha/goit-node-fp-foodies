70691d0b2611a84bbabc44c53e5f322b75b3e99d
svitlarp <svitlanka.ripa@gmail.com>
2026-03-11 00:18:46 +0100

    Public User Registration and Login Endpoints
    feat:
    - register and login endpoints
    - create user model
    Fixes #174

---
+++ backend/migrations/20260310140000-create-users.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("users", {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            avatar: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            token: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            verify: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    async down(queryInterface) {
+        await queryInterface.dropTable("users");
+    },
+};
