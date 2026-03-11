0bbd3fd8a656d24c0069ccedd0966550e5d8eb5f
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-03-11 22:02:47 +0200

    feat: add private create and delete recipe endpoints

--- backend/routes/recipesRouter.js
+++ backend/routes/recipesRouter.js
@@ -1,9 +1,34 @@
 import { Router } from "express";
-import { getRecipes, getRecipeById } from "../controllers/recipesControllers.js";
+import {
+    getRecipes,
+    getRecipeById,
+    createRecipe,
+    deleteRecipe,
+} from "../controllers/recipesControllers.js";
+import { createRecipeSchema } from "../schemas/recipeSchemas.js";
 
 const recipesRouter = new Router();
 
+// временная заглушка auth чтобы работало пока не сделают authenticate
+// когда будет авторизация - убрать сщтые и добавить import следующий
+// import authenticate from "../helpers/authenticate.js"; в шапке файла
+const authenticate = (req, res, next) => {
+    req.user = { id: 1 }; // fake user
+    next();
+};
+
 recipesRouter.get("/", getRecipes);
 recipesRouter.get("/:id", getRecipeById);
+recipesRouter.post(
+    "/",
+    authenticate,
+    validateBody(createRecipeSchema),
+    createRecipe
+);
+recipesRouter.delete(
+    "/:id",
+    authenticate,
+    deleteRecipe
+);
 
 export default recipesRouter;
