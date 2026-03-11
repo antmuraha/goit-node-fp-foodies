f4d577e564183910807ff059f16b149630ab29fd
Viacheslav Opryshko <veo8888@gmail.com>
2026-03-12 00:13:26 +0100

    141 - [BE-USERS-05] Task: Other User Detailed Profile Endpoint

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentUser, getFollowers, getFollowing } from "../controllers/usersControllers.js";
+import { getCurrentUser, getFollowers, getFollowing, getOtherUser } from "../controllers/usersControllers.js";
 import { validatePaginationParams } from "../helpers/validatePaginationParams.js";
 import authenticate from "../middleware/authenticate.js";
 
@@ -8,5 +8,6 @@ const usersRouter = Router();
 usersRouter.get("/me", authenticate, getCurrentUser);
 usersRouter.get("/me/followers", authenticate, validatePaginationParams, getFollowers);
 usersRouter.get("/me/following", authenticate, validatePaginationParams, getFollowing);
+usersRouter.get("/:id", authenticate, getOtherUser);
 
 export default usersRouter;
