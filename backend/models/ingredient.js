661ea656216c56831ccc2422f19699834f140b84
Dmytro <116299166+ChyrkinD@users.noreply.github.com>
2026-03-11 13:57:43 +0100

    177 catalog ingredients list task public ingredients list endpoint (#201)

---
+++ backend/models/ingredient.js
@@ -0,0 +1,39 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class Ingredient extends Model {
+    static associate(models) {
+      if (models.RecipeIngredient) {
+        Ingredient.hasMany(models.RecipeIngredient, { foreignKey: "ingredientId" });
+      }
+    }
+  }
+  Ingredient.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Ingredient",
+      tableName: "ingredients",
+    },
+  );
+  return Ingredient;
+};
