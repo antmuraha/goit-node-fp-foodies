0e8136b4f5e9ed3ef8414f024369c7cb5f4c1e06
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-03-12 21:19:38 +0200

    feat: implement recipes favorites endpoints (add, remove, list)

--- backend/routes/recipesRouter.js
+++ backend/routes/recipesRouter.js
@@ -6,6 +6,9 @@ import {
   deleteRecipe,
   getOwnRecipes,
   getPopularRecipes,
+  addFavorite,
+  removeFavorite,
+  listFavorites,
 } from "../controllers/recipesControllers.js";
 import { createRecipeSchema } from "../schemas/recipeSchemas.js";
 import { validateBody } from "../helpers/validateBody.js";
@@ -14,8 +17,8 @@ const recipesRouter = new Router();
 
 // todo: temporary auth stub to work until authenticate
 // when authenticate will be ready - remove the stub (const authenticate) and add import as follows
-// import authenticate from ".. /helpers/authenticate.js";
-// in the top
+// import authenticate from ".. /middleware/authenticate.js";
+// in the import section
 const authenticate = (req, res, next) => {
   req.user = { id: 1 }; // fake user
   next();
@@ -24,8 +27,11 @@ const authenticate = (req, res, next) => {
 recipesRouter.get("/", getRecipes);
 recipesRouter.get("/popular", getPopularRecipes);
 recipesRouter.get("/own", authenticate, getOwnRecipes);
+recipesRouter.get("/favorites", authenticate, listFavorites);
 recipesRouter.get("/:id", getRecipeById);
 recipesRouter.post("/", authenticate, validateBody(createRecipeSchema), createRecipe);
 recipesRouter.delete("/:id", authenticate, deleteRecipe);
+recipesRouter.post("/:id/favorite", authenticate, addFavorite);
+recipesRouter.delete("/:id/favorite", authenticate, removeFavorite);
 
 export default recipesRouter;
