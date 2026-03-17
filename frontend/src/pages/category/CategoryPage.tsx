13c471f6c7829c6566ee13f0d31b0bb854db63e4
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-03-17 20:59:05 +0200

    fix styles

--- frontend/src/pages/category/CategoryPage.tsx
+++ frontend/src/pages/category/CategoryPage.tsx
@@ -3,22 +3,20 @@ import { useParams } from "react-router-dom";
 import { CategoryRecipesGrid } from "../../features/category-recipes-grid";
 
 export const CategoryPage = (): ReactElement => {
-    const { id: categoryId } = useParams();
+  const { id: categoryId } = useParams();
 
-    return (
-        <>
-            <header>
-                <h1>Category page</h1>
-            </header>
-            <main>
-                <aside>
-                    {/* TODO: FE-CATEGORY-02 — category filters */}
-                </aside>
-                <section>
-                    {/* TODO: FE-CATEGORY-03 — pass pagination prop once ready */}
-                    <CategoryRecipesGrid categoryId={Number(categoryId)} />
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <h1>Category page</h1>
+      </header>
+      <main>
+        <aside>{/* TODO: FE-CATEGORY-02 — category filters */}</aside>
+        <section>
+          {/* TODO: FE-CATEGORY-03 — pass pagination prop once ready */}
+          <CategoryRecipesGrid categoryId={Number(categoryId)} />
+        </section>
+      </main>
+    </>
+  );
 };
