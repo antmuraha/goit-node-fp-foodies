657af848ecb1ef43a24a3b007f21e6659fc27eae
muraha <ruslan.muraha19@gmail.com>
2026-03-19 17:28:09 +0200

    Be 00 centralize base image url with default value (#308)
    * feat(constants): centralize base image URL configuration and update references
    
    * fix(constants): standardize quotes for base image URL definition
    
    * fix(controllers, services): handle null values for base image URL in responses

--- backend/controllers/categoriesControllers.js
+++ backend/controllers/categoriesControllers.js
@@ -4,7 +4,7 @@ import { listCategories } from "../services/categoriesServices.js";
 export const getCategories = async (req, res, next) => {
   try {
     const categories = await listCategories();
-    res.json(categories.map(({ id, name, image }) => ({ id, name, image: image ? baseImageUrl + image : null })));
+    res.json(categories.map(({ id, name, image }) => ({ id, name, image: image ? `${baseImageUrl}${image}` : null })));
   } catch (err) {
     next(err);
   }
