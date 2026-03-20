2aa3f718fb9500f402e79ba2ce3f509ed231db19
Dmytro Chyrkin <probnie.blogi@gmail.com>
2026-03-20 14:43:51 +0100

    added-404-page

--- frontend/src/pages/not-found/NotFoundPage.tsx
+++ frontend/src/pages/not-found/NotFoundPage.tsx
@@ -1,5 +1,14 @@
 import type { ReactElement } from "react";
-
+import styles from "./NotFoundPage.module.css";
 export const NotFoundPage = (): ReactElement => {
-  return <main>Page not found</main>;
+  return (
+    <main className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Oops! Page not found</h2>
+        <p className={styles.description}>
+          The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
+        </p>
+      </div>
+    </main>
+  );
 };
