c0e771051f266b131970a32f3290aaefa29f331f
Rostyslav Rachynskyi <31695066+rrostyslav@users.noreply.github.com>
2026-03-22 18:44:02 +0200

    381 fix burger menu (#382)

--- frontend/src/widgets/header/BurgerMenu/BurgerMenu.tsx
+++ frontend/src/widgets/header/BurgerMenu/BurgerMenu.tsx
@@ -7,14 +7,25 @@ import { APP_ROUTES } from "../../../shared/constants/routes";
 import { HeaderNavLink } from "../HeaderNavLink";
 import heroFoodMain from "../../../assets/images/hero-food-main.webp";
 import heroFoodSmall from "../../../assets/images/hero-food-small.webp";
+import { useState } from "react";
 
 type ButtonMenuProps = {
   variant: "light" | "dark";
 };
 
 export const BurgerMenu = ({ variant }: ButtonMenuProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpenMenu = (open: boolean) => {
+    setIsOpen(open);
+  };
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Dialog.Root>
+    <Dialog.Root open={isOpen} onOpenChange={handleOpenMenu}>
       <Dialog.Trigger asChild>
         <button type="button" className={styles.openBurgerMenuBtn}>
           <Icon name="burger-menu" color={variant === "dark" ? "color-white" : "color-main"} size={28} />
@@ -36,12 +47,14 @@ export const BurgerMenu = ({ variant }: ButtonMenuProps) => {
               </Dialog.Close>
             </div>
             <div className={styles.menu}>
-              <Dialog.Close>
-                <HeaderNavLink fullWidth label="Home" path={APP_ROUTES.HOME} variant="dark" />
-              </Dialog.Close>
-              <Dialog.Close>
-                <HeaderNavLink fullWidth label="Add Recipe" path={APP_ROUTES.RECIPE_ADD} variant="dark" />
-              </Dialog.Close>
+              <HeaderNavLink onClick={handleCloseMenu} fullWidth label="Home" path={APP_ROUTES.HOME} variant="dark" />
+              <HeaderNavLink
+                onClick={handleCloseMenu}
+                fullWidth
+                label="Add Recipe"
+                path={APP_ROUTES.RECIPE_ADD}
+                variant="dark"
+              />
             </div>
             <div className={styles.images} aria-hidden="true">
               <img src={heroFoodMain} alt="" className={styles.imageMain} />
